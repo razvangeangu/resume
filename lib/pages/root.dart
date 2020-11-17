0bbae8299511a6c57b9fe3c80115959381da459d
MihaiTSandu <stmihai98@gmail.com>
2020-11-17 19:31:56 +0200

    style(#1): modified the structure

--- lib/pages/root.dart
+++ lib/pages/root.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class RootPage extends StatefulWidget {
   RootPage({Key key}) : super(key: key);
@@ -7,8 +8,87 @@ class RootPage extends StatefulWidget {
   _RootPageState createState() => _RootPageState();
 }
 
-Widget addEducation(String institutionName, String timePeriod,
-    String degreeType, String description) {
+// ##############  <HEADER> #########################
+
+Widget header() {
+  Widget _information() {
+    return Padding(
+      padding: EdgeInsets.only(left: 20.0),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Text(
+          'John Doe',
+          style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          'Student',
+          style: TextStyle(fontSize: 18.0),
+        ),
+        Row(children: [
+          Padding(
+            padding: EdgeInsets.only(top: 8.0),
+            child: CircleAvatar(
+              radius: 16.0,
+              child: SvgPicture.asset('assets/facebook.svg'),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 12.0, top: 8.0),
+            child: CircleAvatar(
+              radius: 16.0,
+              child: SvgPicture.asset('assets/github.svg'),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 12.0, top: 8.0),
+            child: CircleAvatar(
+              radius: 16.0,
+              child: SvgPicture.asset('assets/linkedin.svg'),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 12.0, top: 8.0),
+            child: CircleAvatar(
+              radius: 16.0,
+              child: SvgPicture.asset('assets/instagram.svg'),
+            ),
+          ),
+        ]),
+      ]),
+    );
+  }
+
+  Widget _avatarImage() {
+    return CircleAvatar(
+      radius: 64.0,
+    );
+  }
+
+  return Container(
+    child: Padding(
+      padding:
+          EdgeInsets.only(top: 64.0, left: 20.0, right: 20.0, bottom: 20.0),
+      child: Row(children: [
+        _avatarImage(),
+        _information(),
+      ]),
+    ),
+  );
+}
+
+// ############################  </HEADER> #######################
+
+Widget section(String title) {
+  return Padding(
+    padding: EdgeInsets.only(bottom: 16.0, top: 20.0, left: 20.0, right: 20.0),
+    child: Text(
+      title,
+      style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w600),
+    ),
+  );
+}
+
+Widget addSection(String institutionName, String timePeriod, String degreeType,
+    String description) {
   return Column(
     crossAxisAlignment: CrossAxisAlignment.start,
     children: [
@@ -21,8 +101,9 @@ Widget addEducation(String institutionName, String timePeriod,
               child: Image(
                 image: NetworkImage(
                     'https://www.iconsdb.com/icons/preview/red/square-xxl.png'),
-                height: 48,
-                width: 48,
+                height: 48.0,
+                width: 48.0,
+                fit: BoxFit.cover,
               ),
             ),
             Padding(
@@ -71,67 +152,10 @@ class _RootPageState extends State<RootPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(
-                  top: 64.0, left: 20.0, right: 20.0, bottom: 20.0),
-              child: Row(children: [
-                CircleAvatar(
-                  radius: 64.0,
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: 20.0),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'John Doe',
-                          style: TextStyle(
-                              fontSize: 24.0, fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          'Student',
-                          style: TextStyle(fontSize: 18),
-                        ),
-                        Row(children: [
-                          Padding(
-                            padding: EdgeInsets.only(top: 8.0),
-                            child: CircleAvatar(
-                              radius: 16,
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(left: 12.0, top: 8.0),
-                            child: CircleAvatar(
-                              radius: 16,
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(left: 12.0, top: 8.0),
-                            child: CircleAvatar(
-                              radius: 16,
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(left: 12.0, top: 8.0),
-                            child: CircleAvatar(
-                              radius: 16,
-                            ),
-                          ),
-                        ]),
-                      ]),
-                ),
-              ]),
-            ),
+            header(),
             Divider(),
-            Padding(
-              padding: EdgeInsets.only(
-                  bottom: 16.0, top: 20.0, left: 20.0, right: 20.0),
-              child: Text(
-                'Education',
-                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w600),
-              ),
-            ),
-            addEducation('University of Bucharest', 'October 2017 - July 2021',
+            section('Education'),
+            addSection('University of Bucharest', 'October 2017 - July 2021',
                 'BA Law', 'very very very very very very very very very nice!'),
           ],
         ),
