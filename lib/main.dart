047af4632c09e0437beffc9f869dddff377b0c7d
MihaiTSandu <stmihai98@gmail.com>
2021-02-02 00:49:18 +0200

    chore: fix firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -8,7 +9,9 @@ import 'package:resume/utils/rg-routes.dart';
 import 'package:resume/utils/rg-shared.dart';
 import 'package:resume/utils/rg-theme.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
